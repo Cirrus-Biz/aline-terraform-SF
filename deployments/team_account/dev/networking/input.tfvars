bd6c28af58010c8143fdaed9b9b47e0b3abddfe6
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-06-15 00:06:50 -0400

    CB-97 feature: updated team_account deployment networking input and variables

--- deployments/team_account/dev/networking/input.tfvars
+++ deployments/team_account/dev/networking/input.tfvars
@@ -25,7 +25,7 @@ private_subnet_numbers = {
                         "us-east-1f" = 12
                         }
 
-# subnet splits form vpc 4 with /16 would give /20 subnet
+# subnet splits from vpc with /16 would give /20 subnets
 subnet_bits_for_split_public = "4"
 subnet_bits_for_split_private = "4"
 
@@ -58,14 +58,6 @@ nacl_public_ingress = [
         cidr_block = "0.0.0.0/0"
         from_port = 0
         to_port = 65535
-    },
-    {
-        protocol = "tcp"
-        rule_no = 100
-        action = "allow"
-        cidr_block = "0.0.0.0/0"
-        from_port = 0
-        to_port = 65535
     }
 ]
 
@@ -78,27 +70,11 @@ nacl_private_egress = [
         cidr_block = "0.0.0.0/0"
         from_port = 0
         to_port = 65535
-    },
-    {
-        protocol = "tcp"
-        rule_no = 100
-        action = "allow"
-        cidr_block = "0.0.0.0/0"
-        from_port = 0
-        to_port = 65535
     }
 ]
 
 # public nacl ingress 
 nacl_public_ingress = [
-    {
-        protocol = "tcp"
-        rule_no = 100
-        action = "allow"
-        cidr_block = "0.0.0.0/0"
-        from_port = 0
-        to_port = 65535
-    },
     {
         protocol = "tcp"
         rule_no = 100
--- deployments/team_account/dev/networking/variables.tf
+++ deployments/team_account/dev/networking/variables.tf
@@ -1,3 +1,6 @@
+# project variables
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 variable "infra_region" {
     type = string
     description = "region for this infrastructure | helps with tagging"
@@ -13,22 +16,26 @@ variable "infra_env" {
 
 variable "project_name" {
   type        = string
-  description = "name of project for tagging | helps with tagging"
+  description = "name of project | helps with tagging"
 }
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+# vpc cidr block
 variable "vpc_cidr_block" {
     type = string
-    description = "vpc cidr block | you should specify this"
+    description = "vpc cidr block | you should specify this in input.tfvars"
     default = "10.0.0.0/16" 
 }
 
 
+# list of AZ to split public subnets into
 variable "public_subnet_numbers" {
   type = map(number)
   description = "map of region AZ to number that should be used for public subnets"
-  # example bellow
-  # default = {
+  # example
+  # public_subnet_numbers = {
   #   "us-east-1a" = 1
   #   "us-east-1b" = 2
   #   "us-east-1c" = 3
@@ -39,11 +46,12 @@ variable "public_subnet_numbers" {
 }
  
 
+# list of AZ to split private subnets into | should start after public number
 variable "private_subnet_numbers" {
   type = map(number)
   description = "map of region AZ to number that should be used for private subnets"
-  # example bellow
-  # default = {
+  # example
+  # private_subnet_numbers = {
   #   "us-east-1a" = 7
   #   "us-east-1b" = 8
   #   "us-east-1c" = 9
@@ -52,5 +60,74 @@ variable "private_subnet_numbers" {
   #   "us-east-1f" = 12
   # }
 }
+
+# variable to set public subnet cidr block size
+# if vpc cidr block is /16 then 4 makes public subnets a /20
+variable "subnet_bits_for_split_public" {
+    type = string
+    description = "number to add onto vpc cidr for public subnet cidr size"
+}
  
  
+# variable to set private subnet cidr block size
+# if vpc cidr block is /16 then 4 makes private subnets a /20
+variable "subnet_bits_for_split_private" {
+    type = string
+    description = "number to add onto vpc cidr for public subnet cidr size"
+}
+
+
+# nacle public egress dynamic block
+variable "nacl_public_egress" {
+    type = list(object({
+        protocol = string
+        rule_no = number
+        action = string
+        cidr_block = string
+        from_port = number
+        to_port = number
+    }))
+    description = "dynamic block of public nacl egress rules"
+}
+
+
+# nacle public ingress dynamic block
+variable "nacl_public_ingress" {
+    type = list(object({
+        protocol = string
+        rule_no = number
+        action = string
+        cidr_block = string
+        from_port = number
+        to_port = number
+    }))
+    description = "dynamic block of public nacl ingress rules"
+}
+
+
+# nacle private egress dynamic block
+variable "nacl_private_egress" {
+    type = list(object({
+        protocol = string
+        rule_no = number
+        action = string
+        cidr_block = string
+        from_port = number
+        to_port = number
+    }))
+    description = "dynamic block of private nacl egress rules"
+}
+
+
+# nacle private ingress dynamic block
+variable "nacl_private_ingress" {
+    type = list(object({
+        protocol = string
+        rule_no = number
+        action = string
+        cidr_block = string
+        from_port = number
+        to_port = number
+    }))
+    description = "dynamic block of private nacl ingress rules"
+}
