641d198104e4928e98be331988b079ce7cfb9a02
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-06-15 20:13:37 -0400

    CB-97 feature: changes

--- deployments/team_account/dev/networking/config.tf
+++ deployments/team_account/dev/networking/config.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
+  profile = "aline"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
 }
