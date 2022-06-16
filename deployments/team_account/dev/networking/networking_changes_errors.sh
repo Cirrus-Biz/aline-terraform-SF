2ac83ee5d9f5ebe81cfec0e3ef0b6b58c5ad5749
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-06-15 22:25:18 -0400

    CB-97 feature: changes

--- deployments/team_account/dev/networking/networking_changes_errors.sh
+++ deployments/team_account/dev/networking/networking_changes_errors.sh
@@ -9,13 +9,17 @@ FILE_PATH=$(dirname "$(realpath $0)")
 # set aws account to aline
 export AWS_PROFILE=default
 
+echo "working dir:"
+pwd
+
 # init to correct state file
-terraform init -backend-config=./deployments/team_account/dev/networking/backend.hcl
-echo "terraform init -backend-config=./deployments/team_account/dev/networking/backend.hcl"
+sudo terraform init -backend-config=./deployments/team_account/dev/networking/backend.hcl
+cd ./deployments/team_account/dev/networking
+echo "new working dir:"
+pwd
 
 # terraform plan that outputs plan to json file to be parsed
-terraform plan -json -var-file=./deployments/team_account/dev/networking/input.tfvars > tfplan_output.json
-echo "terraform plan -json -var-file=./deployments/team_account/dev/networking/input.tfvars > tfplan_output.json"
+# terraform plan -json -var-file=./deployments/team_account/dev/networking/input.tfvars > tfplan_output.json
 
 # greps plan output for errors
 error_check=$(grep -o 'error' ./tfplan_output.json)
