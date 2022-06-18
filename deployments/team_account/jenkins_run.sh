739b9a94141b4ba533ef06be32d308a9fe348a86
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-06-18 00:02:09 -0400

    changed where files are for pipeline

--- deployments/team_account/jenkins_run.sh
+++ deployments/team_account/jenkins_run.sh
@@ -4,13 +4,9 @@
 if [[ $GIT_BRANCH == "origin/testing_jenkins" ]]
     then
         cd  dev/networking && ./networking_changes.sh
-
-        if [ $? -eq 0 ]; then
-            echo "built"
-        else
+        if [ $? -ne 0 ]; then
             exit 1
         fi
-
         echo "NEW SCRIPT WORKED"
     else
         echo "NO BRANCH HERE"
