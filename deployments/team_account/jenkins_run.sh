a67d8487b5834bb7e804d2f26ab631bf2580794a
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-06-18 11:57:00 -0400

    CB-97 feature:

--- deployments/team_account/jenkins_run.sh
+++ deployments/team_account/jenkins_run.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 # if dev branch will run all relevant child scripts and check outputs
-if [[ $GIT_BRANCH == "origin/dev" ]]
-then
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+
+    # dev networking state file
+    cd  dev/networking && ./networking_changes.sh
+    if [[ $? -ne 0 ]]
+    then
+        dev_networking="failed"
+    fi
+elif [[ $GIT_BRANCH == "origin/_all_branch_webhook_test" ]]; then
 
     # dev networking state file
     cd  dev/networking && ./networking_changes.sh
@@ -11,10 +18,12 @@ then
         dev_networking="failed"
     fi
 
+else
+    echo "NO BRANCH MATCH"
+    exit 1
 fi
 
 
-#something
 # checks all exit codes and if any failed will fail the stage of Jenkins pipeline
 if [[ $dev_networking == "failed" ]]
 then
