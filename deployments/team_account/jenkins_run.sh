8a0d144902c87205c3328026168e5bc9d6c22c68
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-06-18 14:09:25 -0400

    CB-97 refactor: adding branch to jenkins_run.sh

--- deployments/team_account/jenkins_run.sh
+++ deployments/team_account/jenkins_run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-# if dev branch will run all relevant child scripts and check outputs
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
+# if main branch will run all relevant child scripts and check outputs
+if [[ $GIT_BRANCH == "origin/main" ]]; then
 
     # dev networking state file
     cd  dev/networking && ./networking_changes.sh
     if [[ $? -ne 0 ]]
     then
-        dev_networking="failed"
+        main_networking="failed"
     fi
 
-elif [[ $GIT_BRANCH == "origin/git_all_branch_webhook_test" ]]; then
+# if dev branch will run all relevant child scripts and check outputs
+elif [[ $GIT_BRANCH == "origin/dev" ]]; then
 
     # dev networking state file
     cd  dev/networking && ./networking_changes.sh
@@ -27,7 +28,7 @@ else
 fi
 
 # checks all exit codes and if any failed will fail the stage of Jenkins pipeline
-if [[ $dev_networking == "failed" ]]
+if [[ $main_networking == "failed" ]] || [[ $dev_networking == "failed" ]]
 then
     exit 1
 fi
