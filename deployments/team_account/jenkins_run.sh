682a8c15f8b96fee6ef40997593e745838f848c7
StephenFreedSS <102398116+StephenFreedSS@users.noreply.github.com>
2022-06-18 14:01:23 -0400

    CB-97 feature:

--- deployments/team_account/jenkins_run.sh
+++ deployments/team_account/jenkins_run.sh
@@ -9,6 +9,7 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     then
         dev_networking="failed"
     fi
+
 elif [[ $GIT_BRANCH == "origin/git_all_branch_webhook_test" ]]; then
 
     # dev networking state file
@@ -19,11 +20,12 @@ elif [[ $GIT_BRANCH == "origin/git_all_branch_webhook_test" ]]; then
     fi
 
 else
-    echo "NO BRANCH MATCH"
+
+    echo "NO BRANCH MATCH TO: $GIT_BRANCH"
     exit 1
+
 fi
 
-# test
 # checks all exit codes and if any failed will fail the stage of Jenkins pipeline
 if [[ $dev_networking == "failed" ]]
 then
